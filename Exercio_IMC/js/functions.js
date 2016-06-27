1666f04e78b836e8b3b8c9de2ee6b8eb8912f2f3
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-06-27 20:40:02 -0300

    Organizando arquivos e refatorando html

---
+++ Exercio_IMC/js/functions.js
@@ -0,0 +1,5 @@
+function alertInputValue() {
+  var valor1 = document.getElementById('peso').value
+  var valor2 = document.getElementById('altura').value
+  document.getElementById('result').innerHTML = (valor1/(valor2*valor2)).toFixed(2) + ' kg/m^2'
+}
