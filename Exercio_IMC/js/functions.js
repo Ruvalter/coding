661b3936efc678ff32e3667043d7736c22412f3a
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-06-28 08:46:24 -0300

    Not submitting on event

--- Exercio_IMC/js/functions.js
+++ Exercio_IMC/js/functions.js
@@ -2,4 +2,5 @@ function alertInputValue() {
   var valor1 = document.getElementById('peso').value
   var valor2 = document.getElementById('altura').value
   document.getElementById('result').innerHTML = (valor1/(valor2*valor2)).toFixed(2) + ' kg/m^2'
+  return false
 }
