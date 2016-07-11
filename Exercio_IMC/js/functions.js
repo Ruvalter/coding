9334870c5affd3a908ea5a629396f92d87beb079
ruvalter <ruvalter@gmail.com>
2016-07-11 20:59:48 -0300

    Added Jquery

--- Exercio_IMC/js/functions.js
+++ Exercio_IMC/js/functions.js
@@ -2,5 +2,6 @@ function alertInputValue() {
   var valor1 = document.getElementById('peso').value
   var valor2 = document.getElementById('altura').value
   document.getElementById('result').innerHTML = (valor1/(valor2*valor2)).toFixed(2) + ' kg/m^2'
+  // frm.reset()
   return false
 }
