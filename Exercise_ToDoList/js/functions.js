58dd37803475bb78dfe9f33e9f341009c3626093
ruvalter <ruvalter@gmail.com>
2016-08-10 22:17:24 -0300

    Organizei melhor a Table

--- Exercise_ToDoList/js/functions.js
+++ Exercise_ToDoList/js/functions.js
@@ -3,7 +3,8 @@ $(document).ready(function ()  {
   var taskArray = [
     {
       name: "tarefa1",
-      date: "data1"
+      date: "data1",
+      checkedDone: false
     }
   ]
 
@@ -19,7 +20,8 @@ $(document).ready(function ()  {
 
         var task = {
          name: value1,
-         date: value2
+         date: value2,
+         checkedDone: false
         }
         // coloca os novos valores no final do vetor
         taskArray.push(task)
@@ -33,43 +35,77 @@ $(document).ready(function ()  {
     return false
   })
 
-  $(".done").click(function() {
-    debugger
-    swal({
-      title: "Good Job",
-      text: "You have concluded another one!",
-      timer: 2000,
-      showConfirmButton: false
-    })
+  $(document).on('click',".done", function() {
 
+    if ($(this).prop("checked")) {
+      swal({
+        title: "Good Job",
+        text: "You have concluded another one!",
+        timer: 1000,
+        showConfirmButton: false
+      })
+    }
 
-
-
+    var index = $(this).data("index")
+    taskArray[index].checkedDone = $(this).prop("checked")
   })
+
+  $(document).on('click',".delete", function() {
+
+    swal({
+      title: "Are you sure?",
+      text: "You will not be able to recover this imaginary file!",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Yes, delete it!",
+      closeOnConfirm: false
+    },
+
+    function(){
+      swal("Deleted!", "Your imaginary file has been deleted.", "success");
+    })
+  })
+
+
 
   function showTask() {
 
     var taskList = ""
     for (var i = 0; i < taskArray.length; i++) {
-      // taskList +=  "<li class='list-group-item'>" + taskArray[i].name +"<span class='date'>"+ taskArray[i].date + "</span></li>"
-      taskList +=  "<tr>" + "<td>" + "<input type='checkbox' class='done'>" + "</td>" + "<td class='task_style'>" + taskArray[i].name + "</td>" + "<td class='task_style'>" + taskArray[i].date + "</td>" + "<td class='date'>" + "<button class='btn btn-danger'><i class='fa fa-bars' aria-hidden='true'></i></button>" + "</td>" + "</tr>"
+      var checked = ""
+      if (taskArray[i].checkedDone) {
+        checked = "checked"
+      }
+
+      taskList +=  "<tr>"
+      taskList += "<td>"
+      taskList += "<input type='checkbox' data-index='"
+      taskList += i
+      taskList += "' class='done'"
+      taskList += checked
+      taskList += " >"
+      taskList += "</td>"
+      taskList += "<td class='task_style'>"
+      taskList += taskArray[i].name
+      taskList += "</td>"
+      taskList += "<td class='task_style'>"
+      taskList += taskArray[i].date
+      taskList += "</td>"
+      taskList += "<td class='date'>"
+      taskList += "<button class='delete btn btn-default'><i class='fa fa-bars' aria-hidden='true'></i></button>"
+      taskList += "</td>"
+      taskList += "</tr>"
     }
 
-    // document.getElementById("toDoList").innerHTML = taskList
-    $("#toDoTable").html(taskList)
+    $(".sortable").html(taskList)
   }
+
+  $( ".sortable" ).sortable();
+  $( ".sortable" ).disableSelection();
 })
 
 
-
-
-
-
-
-function dateVerify() {
-
-
-}
 
 
 
